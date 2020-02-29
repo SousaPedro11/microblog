0d27cac95c7f7d72ed18f790ef4211485d0df5dd
Pedro Sousa <ppls2106@gmail.com>
2020-02-29 13:41:08 -0300

    add models

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config(object):
-    # SECRET_KEY = os.environ.get('SECRET_KEY') or 'Pedro@12345'
-    SECRET_KEY = os.urandom(16) or 'Pedro@12345'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Pedro@12345'
+    # SECRET_KEY = os.urandom(16) or 'Pedro@12345'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
